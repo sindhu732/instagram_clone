67e87f02868dbf36c5f776e91429e4107948b6e1
Sindhu <sindhu732@gmail.com>
2017-12-06 04:23:02 -0500

    broken listadapter's use in HomeActivity

--- app/src/main/java/com/cornell/sindhu/instagram/Home/HomeActivity.java
+++ app/src/main/java/com/cornell/sindhu/instagram/Home/HomeActivity.java
@@ -2,17 +2,31 @@ package com.cornell.sindhu.instagram.Home;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 
 import com.cornell.sindhu.instagram.Account.AccountActivity;
+import com.cornell.sindhu.instagram.Models.Post;
+import com.cornell.sindhu.instagram.Models.User;
 import com.cornell.sindhu.instagram.Utils.BottomNavigationViewHelper;
 import com.cornell.sindhu.instagram.R;
+import com.cornell.sindhu.instagram.Utils.MainFeedListAdapter;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -20,7 +34,9 @@ public class HomeActivity extends AppCompatActivity {
     private static final int ACTIVITY_NUMBER = 0;
 
     private FirebaseAuth mAuth;
-
+    private FirebaseDatabase mDatabase;
+    private ListView mFeed;
+    private ListAdapter listAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +46,38 @@ public class HomeActivity extends AppCompatActivity {
 
         setupBottomNavigationBar();
         mAuth = FirebaseAuth.getInstance();
+        mDatabase = FirebaseDatabase.getInstance();
+
+        mFeed = findViewById(R.id.myFeed);
+
+        downloadMyFeed();
+    }
+
+    private void downloadMyFeed() {
+
+        final List<Post> posts = new ArrayList<>();
+        DatabaseReference myRef = mDatabase.getReference("posts");
+        final FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Log.d(TAG, "got Data " + dataSnapshot.getChildren().toString());
+
+                for(DataSnapshot snapshot: dataSnapshot.getChildren()) {
+                    posts.add(snapshot.getValue(Post.class)); // TODO: Need to pass in Post object to posts from Java Object. Clue: No setter/field for -L-dtEzsYFY_YdhfFBO2 found on class com.cornell.sindhu.instagram.Models.Post
+                    Log.d(TAG, "got Post: " + snapshot.getValue().toString());
+                }
+
+                //listAdapter = new MainFeedListAdapter(HomeActivity.this, R.layout.layout_feed_list_item, posts);
+                //mFeed.setAdapter(listAdapter);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
     }
 
     private void setupBottomNavigationBar() {
---
+++ app/src/main/java/com/cornell/sindhu/instagram/Utils/MainFeedListAdapter.java
@@ -0,0 +1,68 @@
+package com.cornell.sindhu.instagram.Utils;
+
+import android.content.Context;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.cornell.sindhu.instagram.Models.Post;
+import com.cornell.sindhu.instagram.R;
+import com.google.firebase.database.DatabaseReference;
+
+import java.util.List;
+
+/**
+ * Created by sindhu on 12/6/17.
+ */
+
+public class MainFeedListAdapter extends ArrayAdapter<Post>{
+
+    private LayoutInflater mInflater;
+    private int mLayoutReference;
+    private Context mContext;
+    private DatabaseReference mReference;
+    private String currentUsername = "";
+
+    public MainFeedListAdapter(@NonNull Context context, @LayoutRes int resource, @NonNull List<Post> objects) {
+        super(context, resource, objects);
+        mInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        mLayoutReference = resource;
+        this.mContext = context;
+    }
+
+    static class PostView{
+        TextView displayName, description, privateStatus, publicStatus;
+        ImageView image;
+        Post post;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+        final PostView holder;
+
+        if(convertView == null) {
+            convertView = mInflater.inflate(mLayoutReference, parent, false);
+            holder = new PostView();
+
+            holder.displayName = (TextView) convertView.findViewById(R.id.displayName);
+            holder.description = (TextView) convertView.findViewById(R.id.description);
+            holder.image = (ImageView) convertView.findViewById(R.id.postImage);
+            holder.privateStatus = (TextView) convertView.findViewById(R.id.privateStatus);
+            holder.publicStatus = (TextView) convertView.findViewById(R.id.publicStatus);
+
+            holder.post = getItem(position);
+        }
+
+        return convertView;
+    }
+
+}
